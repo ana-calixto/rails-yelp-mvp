7f0cdbcc242c2acd8b82eba2e45e738b17ce4cf5
Ana Calixto <aninhacalixto@gmail.com>
2020-11-18 15:47:01 -0300

    restaurants list

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,40 @@
+class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show]
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @reviews = @restaurant.reviews
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    redirect_to restaurants_url, notice: 'Restaurant was successfully destroyed.'
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "faker"
+
+Restaurant.destroy_all
+puts "destroying all restaurants"
+10.times do
+  restaurant = Restaurant.create!(name: Faker::Restaurant.name,
+                     address: Faker::Address.full_address,
+                     category: %w(chinese italian japanese french belgian).sample,
+                     phone_number: Faker::PhoneNumber.phone_number)
+                     puts "#{restaurant.name}, created"
+end
